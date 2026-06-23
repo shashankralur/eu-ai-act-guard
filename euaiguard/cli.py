7b21d0e730880f149c9ed9a5b022df145dd6a424
Shashank R Alur <shashankr1931@gmail.com>
2026-06-23 08:36:31 +0530

    command created init and report

--- euaiguard/cli.py
+++ euaiguard/cli.py
@@ -1,23 +1,14 @@
-# import typer
-
-# app = typer.Typer()
-
-
-# @app.callback()
-# def main():
-#     print("Hello from EU AI Guard")
-
-
-# if __name__ == "__main__":
-#     app()
 import typer
 
 app = typer.Typer()
 
-
-@app.callback(invoke_without_command=True)
-def main():
-    print("Hello from EU AI Guard")
+@app.command()
+def init():
+    print("Welcome to Eu AI Guard")
+    
+@app.command()
+def report():
+    print("Report Generating...")
 
 
 if __name__ == "__main__":
