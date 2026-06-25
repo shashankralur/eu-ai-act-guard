efbfb0f2ce44668d921ad20a47b620e02f6d67a0
Shashank R Alur <shashankr1931@gmail.com>
2026-06-25 19:51:45 +0530

    created commands through folder structure

--- euaiguard/cli.py
+++ euaiguard/cli.py
@@ -1,22 +1,12 @@
 import typer
 
+from euaiguard.commands.init import init
+from euaiguard.commands.report import report
+
 app = typer.Typer()
 
-@app.command()
-def init(
-    project_name: str,
-    verbose: bool = False
-):
-    """
-    Initialize a new AI project for compliance tracking.
-    """
-
-    print(f"Project: {project_name}")
-    
-@app.command()
-def report():
-    print("Report Generating...")
-
+app.command()(init)
+app.command()(report)
 
 if __name__ == "__main__":
     app()
